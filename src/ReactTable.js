a19cc743e4177de306b474ea1f1a6f9eb2fdbd53
Wenjie Chen <cwjcourage@gmail.com>
2015-05-06 14:25:12 -0400

    Update ReactTable.js
    add a null check for onRightClick

--- src/ReactTable.js
+++ src/ReactTable.js
@@ -245,7 +245,7 @@ var Row = React.createClass({
             styles[attrname] = this.props.extraStyle[attrname];
         }
         return (<tr onClick={this.props.onSelect.bind(null, this.props.data)}
-                    onContextMenu={this.props.onRightClick.bind(null, this.props.data)}
+                    onContextMenu={this.props.onRightClick ? this.props.onRightClick.bind(null, this.props.data) : null}
                     className={classes} style={styles}>{cells}</tr>);
     }
 });
